4e4bf3681646538c5afb0f3079b01be470ffd41a
abenezeryakob <abenezer@kth.se>
2017-04-24 22:35:18 +0200

    test data

--- project/cmsplugin/Children_view/childern_view_model.py
+++ project/cmsplugin/Children_view/childern_view_model.py
@@ -7,8 +7,8 @@ class SelectOurChildren(CMSPlugin):
     name = models.CharField(max_length=255, blank=True, null=True)
     our_children = models.ManyToManyField(OurChildren)
 
-    #def copy_relations(self, old_instance):
-        #self.our_children = old_instance.our_children.all()
+    def copy_relations(self, old_instance):
+        self.our_children = old_instance.our_children.all()
 
     def get_data(self):
         return (self.our_children.objects.all())
